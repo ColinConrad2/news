d38dc8efed2abf0b69b681eb743ff393fa9de41f
Colin Conrad <aceofspadescolin@gmail.com>
2024-11-04 19:23:42 -0700

    captcha

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,19 @@
+#accounts/forms.py
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms
+from captcha.fields import CaptchaField
+
 
 class CustomUserCreationForm(UserCreationForm):
+    captcha = CaptchaField()
     class Meta:
         model = CustomUser
         fields = (
             "username",
             "email",
             "age",
+            "captcha",
         )
 
 class CustomeUserChangeForm(UserChangeForm):
--- django_project/urls.py
+++ django_project/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("articles/", include("articles.urls")),
     path("", include("pages.urls")),
+    path('captcha/', include('captcha.urls')),
 ] + debug_toolbar_urls()
 
